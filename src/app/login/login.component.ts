cad530a4386cbf5ab6bd063c244f4a2631892c00
jjellis <jjjr1234@aol.com>
2019-10-16 19:41:09 -0500

    inital

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,   
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: '',
+      password:''
+    });
   }
 
-}
+  onSubmit(loginData){
+    if(this.loginForm.invalid){
+      return;
+    }
+    this.authService
+    .login(loginData.email, loginData.password) 
+        .pipe(first())
+        .subscribe(
+            data => {
+                this.router.navigateByUrl("/");
+            },
+            error => {
+                console.log(error);
+            });
+          }
+        }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,11 +22,15 @@ export class AuthService {
     return this.currentUserSubject.value;
   }
 
-  login(username: string, password: string) {
+  login(email: string, password: string) {
     return this.http
-      .post<any>(`/users/authenticate`, { username, password })
+      .post<any>("https://loacalhost:44367/api/auth/login",{
+        email: email,
+        password:password
+      })
       .pipe(
         map(user => {
+          console.log(user);
           // store user details and jwt token in local storage to keep user logged in between page refreshes
           localStorage.setItem("currentUser", JSON.stringify(user));
           this.currentUserSubject.next(user);
